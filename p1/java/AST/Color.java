15a64e7610adf5fb381f0cf57233c07158ce9f8e
javichen18 <javierchen92@gmail.com>
2023-04-09 12:43:18 +0200

    p1 COMPLETED, dont know what to do with color

--- p1/java/AST/Color.java
+++
@@ -1,9 +0,0 @@
-package AST;
-
-public class Color implements Exp {
-    public final int n;
-
-    public Color(int n) {
-        this.n = n;
-    }
-}
--- p1/java/AST/ConstantePi.java
+++
@@ -1,9 +0,0 @@
-package AST;
-
-public class ConstantePi implements Exp {
-    public final float n;
-
-    public ConstantePi(float n) {
-        this.n = n;
-    }
-}
