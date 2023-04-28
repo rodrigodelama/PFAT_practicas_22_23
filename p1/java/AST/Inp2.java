59cb6551eb935d7c8a159012217c20e988e73f16
javichen18 <javierchen92@gmail.com>
2023-04-28 18:23:29 +0200

    DONE!!!!!

--- p1/java/AST/Inp2.java
+++ p1/java/AST/Inp2.java
@@ -14,7 +14,7 @@ public class Inp2 implements Inp {
     }
     public int getArgsLength() {
         // No es necesario hacer nada ya que la producción lambda no tiene efectos sobre el atributo heredado
-        return 0;
+        return -1; //para el caso de no argumentos
     }
     public void generateCode(BufferedWriter w, String indent) throws IOException {
         // No es necesario hacer nada ya que la producción lambda no tiene efectos sobre el atributo heredado
