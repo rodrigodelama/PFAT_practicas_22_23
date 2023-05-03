f19a01d97f9486a5df7b586881897de640e1f68d
Rodrigo De Lama <rodrigo.delama@icloud.com>
2023-05-03 14:33:45 +0200

    part of stage 3

--- p1/java/AST/Plot.java
+++ p1/java/AST/Plot.java
@@ -7,23 +7,25 @@ import Compiler.*;
 import Errors.*;
 
 public class Plot implements Sentencia {
+    public final String id;
     public final Exp e1; //x
     public final Exp e2; //y
     public final int color;
 
-    public Plot(Exp e1, Exp e2, int color) {
+    public Plot(String id, Exp e1, Exp e2, int color) {
+        this.id = id; //ident
         this.e1 = e1;
         this.e2 = e2;
         this.color = color;
-        
     }
 
     public int computeStTyp() throws CompilerExc {
-        int t1, t2;
+        int t0, t1, t2;
+        t0 = SymbolTable.getType(id);
         t1 = e1.computeTyp();
         t2 = e2.computeTyp();
 
-        if((t1 == Typ.t_int) && (t2 == Typ.t_int)) {
+        if((t0 == Typ.t_color) && (t1 == Typ.t_int) && (t2 == Typ.t_int)) {
             return Typ.t_void;
         } else {
             throw new TypExc("Error en Plot");
--- p1/java/Compiler/Typ.java
+++ p1/java/Compiler/Typ.java
@@ -4,6 +4,7 @@ public class Typ {
     public static final int t_int = 200;
     public static final int t_bool = 300;
     public static final int t_real = 400;
+    public static final int t_color = 500; //stage 3
     public static final int t_void = 600;
 
     public static String typToString(int typ) {
@@ -15,6 +16,9 @@ public class Typ {
         }
         if(typ == t_real) {
             return "real";
+        }
+        if(typ == t_color) { //stage 3
+            return "color";
         }
         return "void";
     }
